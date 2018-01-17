6dd0eaf84acf544d7c0421916f344717dc2739a3
yunjinkyoung <jk4877@naver.com>
2018-01-17 18:40:57 +0900

    test

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to IL.D. HomePage</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          안녕하세요, IL.D. 홍보 test페이지 입니다.
         </p>
       </div>
     );
